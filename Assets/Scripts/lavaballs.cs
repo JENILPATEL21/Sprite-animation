03955b9b96c820e59d2a68b0aafef488d7b78edc
jenilpatel02 <JENIL.MONPARA@YUDIZ.COM>
2023-04-10 12:06:54 +0530

    cc

---
+++ Assets/Scripts/lavaballs.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lavaballs : MonoBehaviour
+{
+    float lavaspeed = 10f;
+    Rigidbody2D ballrb;
+    PlayerBehaviour player;
+    float horizontalspeed;
+    bool faceright = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballrb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerBehaviour>();
+        horizontalspeed = player.transform.localScale.x * lavaspeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ballrb.velocity = new Vector2(horizontalspeed, 0f);
+        
+        if (horizontalspeed > 0 && !faceright)
+        {
+            Flip(horizontalspeed);
+        }
+
+        else if (horizontalspeed < 0 && faceright)
+        {
+            Flip(horizontalspeed);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag =="Border")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void Flip(float x)
+    {
+        faceright = !faceright;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
